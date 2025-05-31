d37e37943f949b361c604dc9e53e6d44a67df20b
Logen11fingers <126648995+Logen10fingers@users.noreply.github.com>
2025-05-31 08:04:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const enterBtn = document.getElementById("enterBtn");
+  const statusParagraph = document.getElementById("status");
+
+  enterBtn.addEventListener("click", function() {
+    const newHeading = document.createElement("h1");
+    newHeading.textContent = "Entered Metaverse";
+    statusParagraph.replaceWith(newHeading);
+  });
+});
